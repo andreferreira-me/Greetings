90798a7e14291126baff938f6fe3462ddfc7b119
André Ferreira <contato@andreferreira.me>
2017-03-06 02:32:44 -0300

    First commit

--- Greetings/Greetings/App.cs
+++ Greetings/Greetings/App.cs
@@ -11,23 +11,7 @@ namespace Greetings
     {
         public App()
         {
-            // The root page of your application
-            var content = new ContentPage
-            {
-                Title = "Greetings",
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
-
-            MainPage = new NavigationPage(content);
+            MainPage = new GreetingsPage();
         }
 
         protected override void OnStart()
---
+++ Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            var MyLabel = new Label();
+            MyLabel.Text = "Greetings, Xamarin.Forms!";
+            this.Content = MyLabel;
+
+            MyLabel.HorizontalTextAlignment = TextAlignment.Center;
+            MyLabel.VerticalTextAlignment = TextAlignment.Center;
+
+            //MyLabel.HorizontalOptions = LayoutOptions.Center;
+            //MyLabel.VerticalOptions = LayoutOptions.Center;
+
+            // Adiciona Padding no iOS para evitar barra de status do device
+
+            Device.OnPlatform(iOS: () =>
+            {
+                Padding = new Thickness(0, 20, 0, 0);
+            });            // O método Device.OnPlatform é muito conveniente e tem a vantagem de trabalhar em
+            // ambos os projetos PCL e SAP.No entanto, você não pode acessar as APIs dentro de
+            // plataformas individuais. Para fazer isso vamos precisar DependencyService.
+        }
+    }
+}
